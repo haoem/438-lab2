23240eb49634388b5fa338a40b2a1a1290422665
Emily Hao <emilymhao@gmail.com>
2024-09-11 17:22:20 -0700

    Initial Commit

---
+++ EmilyHao-Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmilyHao-Lab2
+//
+//  Created by Hao, Emily on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
